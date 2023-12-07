d46765cc6c63ff1ee7065590fd695e22c62b59d6
Václav Chlumský <chlumskyvaclav@gmail.com>
2023-12-07 11:55:57 +0100

    add cleaner to the package fix

--- release-rpm.sh
+++ release-rpm.sh
@@ -9,6 +9,7 @@ cp hook_softwalltime_predictor.py softwalltime-predictor-$VERSION/
 cp pbs_softwalltime_predictor softwalltime-predictor-$VERSION/
 cp softwalltime.conf softwalltime-predictor-$VERSION/
 cp softwalltime-predictor.spec softwalltime-predictor-$VERSION/
+cp softwalltime_predictor-cleaner softwalltime-predictor-$VERSION/
 cp softwalltime_predictor-detector softwalltime-predictor-$VERSION/
 cp softwalltime_predictor-runtime_saver softwalltime-predictor-$VERSION/
 cp softwalltime_psql.py softwalltime-predictor-$VERSION/
